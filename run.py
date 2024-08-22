c4e12fc0e3efe8176860a06d6f588996f93c4d01
work <ovanes12@icloud.com>
2024-08-22 15:50:47 +0300

    fix: исправлена структура

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+
 from config import TOKEN
 from app.handlers import router
 
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
-
 async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
